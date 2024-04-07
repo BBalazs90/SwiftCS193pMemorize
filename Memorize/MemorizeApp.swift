6007582c2dfc7462e1348a039964b3276f6e03aa
Balazs Balogh <balazs.bb.balogh@gmail.com>
2024-04-07 20:30:11 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Balazs on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
